54091a172f2d7ccb944db50da020c9e2532115b1
shaun <shaun@bluemooncreative.co.uk>
2024-07-02 21:43:48 +0100

    Add vercel speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import {ReCaptchaProvider} from "next-recaptcha-v3";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "../styles/styles.scss";
 function MyApp({ Component, pageProps }) {
   return (
-      <ReCaptchaProvider>
-        <Component {...pageProps} />
-      </ReCaptchaProvider>
+      <>
+          <ReCaptchaProvider>
+              <Component {...pageProps} />
+          </ReCaptchaProvider>
+          <SpeedInsights/>
+      </>
   );
 }
 
